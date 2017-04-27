4c3b8b51163438fc1b83a36927bd67180ee40177
Tom McClean <tommcclean@hotmail.co.uk>
2017-04-27 21:01:47 +0100

    Add Initial Models Project with 'SyncProfile' Stub

---
+++ HarmoniseDB.Models/SyncProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HarmoniseDB.Models
+{
+    public class SyncProfile
+    {
+        public string SyncProfileName { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
