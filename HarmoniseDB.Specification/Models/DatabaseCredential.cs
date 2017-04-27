2e14f74559a3972c88a78b93d0368931f8eb59f8
Tom McClean <tommcclean@hotmail.co.uk>
2017-04-27 21:11:06 +0100

    Add Stub 'HarmoniseDB.Specification' Project

---
+++ HarmoniseDB.Specification/Models/DatabaseCredential.cs
@@ -0,0 +1,23 @@
+﻿namespace HarmoniseDB.Specification.Models
+{
+    public class DatabaseCredential
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public string SQLServerInstance { get; set; } = "localhost";
+
+        public string InitialCatalog { get; set; } = "master";
+
+        public bool IntegratedSecurity { get; set; } = true;
+
+        public string UserId { get; set; }
+
+        public string UserPassword { get; set; }
+    }
+}
---
+++ HarmoniseDB.Specification/Models/SyncProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HarmoniseDB.Specification.Models
+{
+    public class SyncProfile
+    {
+        public string SyncProfileName { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+        public DatabaseCredential SourceDatabase { get; set; }
+
+        public DatabaseCredential TargetDatabase { get; set; }
+    }
+}
